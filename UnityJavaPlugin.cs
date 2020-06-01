0afe7dd24dffb696ed122c3ea580f69ec332e549
himanshh974 <60419105+himanshh974@users.noreply.github.com>
2020-06-01 15:06:18 +0530

    Add files via upload

---
+++ UnityJavaPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityJavaPlugin : MonoBehaviour
+{
+    public Text myText;
+  
+    public void changedTextToFuck(string value)
+    {
+        myText.text = value;
+    }
+   
+    
+}
